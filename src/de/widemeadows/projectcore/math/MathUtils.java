d6dcc0bd8ef98cb3ab796784bc70da720742b8cd
Markus Mayer <development@widemeadows.de>
2011-11-15 03:07:56 +0100

    Vergleich auf null hinzugefügt

--- src/de/widemeadows/projectcore/math/MathUtils.java
+++ src/de/widemeadows/projectcore/math/MathUtils.java
@@ -12,12 +12,50 @@ public class MathUtils {
 
 	/**
 	 * Überprüft, ob zwei Floatwerte identisch unter Beachtung eines Deltawertes sind
+	 *
 	 * @param value Der zu testende Wert
 	 * @param referenceValue Der Vergleichswert
-	 * @param delta Der Deltawert
+	 * @param delta Der Deltawert >= 0
 	 * @return <code>true</code>, wenn beide Werte identisch sind
 	 */
 	public static boolean equals(float value, float referenceValue, float delta) {
+		assert delta >= 0;
 		return Math.abs(value - referenceValue) <= delta;
+	}
+
+	/**
+	 * Überprüft, ob ein Floatwerte identisch mit null ist
+	 *
+	 * @param value          Der zu testende Wert
+	 * @param delta          Der Deltawert >= 0
+	 * @return <code>true</code>, wenn beide Werte identisch sind
+	 */
+	public static boolean isZero(float value, float delta) {
+		assert delta >= 0;
+		return Math.abs(value) <= delta;
+	}
+
+	/**
+	 * Überprüft, ob ein Floatwerte größer als null ist
+	 *
+	 * @param value Der zu testende Wert
+	 * @param delta Der Deltawert >= 0
+	 * @return <code>true</code>, wenn beide Werte identisch sind
+	 */
+	public static boolean isLargerZero(float value, float delta) {
+		assert delta >= 0;
+		return value > delta;
+	}
+
+	/**
+	 * Überprüft, ob ein Floatwerte kleiner als null ist
+	 *
+	 * @param value Der zu testende Wert
+	 * @param delta Der Deltawert >= 0
+	 * @return <code>true</code>, wenn beide Werte identisch sind
+	 */
+	public static boolean isSmallerZero(float value, float delta) {
+		assert delta >= 0;
+		return value < -delta;
 	}
 }
--- src/de/widemeadows/projectcore/math/tests/UtilsTests.java
+++ src/de/widemeadows/projectcore/math/tests/UtilsTests.java
@@ -28,4 +28,51 @@ public class UtilsTests {
 		assertTrue(MathUtils.equals(0, -0.001f, 0.002f));
 	}
 
+	/**
+	 * Überprüft die Equals-Funktion
+	 */
+	@Test
+	public void isZero() {
+		assertTrue(MathUtils.isZero(0, 0));
+		assertTrue(MathUtils.isZero(1, 1));
+		assertTrue(MathUtils.isZero(-1, 1));
+		assertTrue(MathUtils.isZero(0.001f, 0.01f));
+		assertTrue(MathUtils.isZero(-0.001f, 0.01f));
+		assertFalse(MathUtils.isZero(0.1f, 0.01f));
+		assertFalse(MathUtils.isZero(-0.1f, 0.01f));
+		assertFalse(MathUtils.isZero(2, 1));
+		assertFalse(MathUtils.isZero(-2, 1));
+	}
+
+	/**
+	 * Überprüft die Equals-Funktion
+	 */
+	@Test
+	public void isLargerZero() {
+		assertFalse(MathUtils.isLargerZero(0, 0));
+		assertFalse(MathUtils.isLargerZero(1, 1));
+		assertFalse(MathUtils.isLargerZero(-1, 1));
+		assertFalse(MathUtils.isLargerZero(0.001f, 0.01f));
+		assertFalse(MathUtils.isLargerZero(-0.001f, 0.01f));
+		assertTrue(MathUtils.isLargerZero(0.1f, 0.01f));
+		assertFalse(MathUtils.isLargerZero(-0.1f, 0.01f));
+		assertTrue(MathUtils.isLargerZero(2, 1));
+		assertFalse(MathUtils.isLargerZero(-2, 1));
+	}
+
+	/**
+	 * Überprüft die Equals-Funktion
+	 */
+	@Test
+	public void isSmallerZero() {
+		assertFalse(MathUtils.isSmallerZero(0, 0));
+		assertFalse(MathUtils.isSmallerZero(1, 1));
+		assertFalse(MathUtils.isSmallerZero(-1, 1));
+		assertFalse(MathUtils.isSmallerZero(0.001f, 0.01f));
+		assertFalse(MathUtils.isSmallerZero(-0.001f, 0.01f));
+		assertFalse(MathUtils.isSmallerZero(0.1f, 0.01f));
+		assertTrue(MathUtils.isSmallerZero(-0.1f, 0.01f));
+		assertFalse(MathUtils.isSmallerZero(2, 1));
+		assertTrue(MathUtils.isSmallerZero(-2, 1));
+	}
 }
