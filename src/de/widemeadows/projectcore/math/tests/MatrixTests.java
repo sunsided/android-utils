07a25c98398037ada6b0c54cf5e84957c84de1ed
Markus Mayer <development@widemeadows.de>
2011-11-15 02:17:25 +0100

    Unit Test funktioniert

--- src/de/widemeadows/projectcore/math/tests/MatrixTests.java
+++ src/de/widemeadows/projectcore/math/tests/MatrixTests.java
@@ -1,44 +1,54 @@
 package de.widemeadows.projectcore.math.tests;
 
 import de.widemeadows.projectcore.math.Matrix4;
-import junit.framework.Assert;
 import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
 
 /**
  * Unit Tests für {@link de.widemeadows.projectcore.math.Matrix4}
  */
 public class MatrixTests {
 
+	/**
+	 * Epsilonwert für Float-Vergleiche
+	 */
+	private static final float Epsilon = 0.00001f;
+
+	/**
+	 * Stellt sicher, dass die Matrix nach Initialisierung eine Identitätsmatrix ist.
+	 */
     @Test
-    public static void testIsIdentity() {
+    public void isIdentity() {
 
         Matrix4 a = Matrix4.createNew();
+	    final float initializationDelta = 0.0f;
 
         // einsen Testen
-        Assert.assertEquals(1, a.getAt(0, 0));
-        Assert.assertEquals(1, a.getAt(1, 1));
-        Assert.assertEquals(1, a.getAt(2, 2));
-        Assert.assertEquals(1, a.getAt(3, 3));
+        assertEquals(1, a.getAt(0, 0), initializationDelta);
+        assertEquals(1, a.getAt(1, 1), initializationDelta);
+        assertEquals(1, a.getAt(2, 2), initializationDelta);
+        assertEquals(1, a.getAt(3, 3), initializationDelta);
 
         // Nullen erste Reihe
-        Assert.assertEquals(0, a.getAt(0, 1));
-        Assert.assertEquals(0, a.getAt(0, 2));
-        Assert.assertEquals(0, a.getAt(0, 3));
+        assertEquals(0, a.getAt(0, 1), initializationDelta);
+        assertEquals(0, a.getAt(0, 2), initializationDelta);
+        assertEquals(0, a.getAt(0, 3), initializationDelta);
 
         // Nullen zweite Reihe
-        Assert.assertEquals(0, a.getAt(1, 0));
-        Assert.assertEquals(0, a.getAt(1, 2));
-        Assert.assertEquals(0, a.getAt(1, 3));
+        assertEquals(0, a.getAt(1, 0), initializationDelta);
+        assertEquals(0, a.getAt(1, 2), initializationDelta);
+        assertEquals(0, a.getAt(1, 3), initializationDelta);
 
         // Nullen dritte Reihe
-        Assert.assertEquals(0, a.getAt(2, 0));
-        Assert.assertEquals(0, a.getAt(2, 1));
-        Assert.assertEquals(0, a.getAt(2, 3));
+        assertEquals(0, a.getAt(2, 0), initializationDelta);
+        assertEquals(0, a.getAt(2, 1), initializationDelta);
+        assertEquals(0, a.getAt(2, 3), initializationDelta);
 
         // Nullen vierte Reihe
-        Assert.assertEquals(0, a.getAt(3, 0));
-        Assert.assertEquals(0, a.getAt(3, 1));
-        Assert.assertEquals(0, a.getAt(3, 2));
+        assertEquals(0, a.getAt(3, 0), initializationDelta);
+        assertEquals(0, a.getAt(3, 1), initializationDelta);
+        assertEquals(0, a.getAt(3, 2), initializationDelta);
     }
 
 }
