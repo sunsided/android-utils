d6308bdc4ec2a7b41cd93bdc676beb85a6dcea11
Markus Mayer <development@widemeadows.de>
2011-11-15 02:40:32 +0100

    Accessoren getestet

--- src/de/widemeadows/projectcore/math/tests/MatrixTests.java
+++ src/de/widemeadows/projectcore/math/tests/MatrixTests.java
@@ -1,7 +1,10 @@
 package de.widemeadows.projectcore.math.tests;
 
 import de.widemeadows.projectcore.math.Matrix4;
+import org.jetbrains.annotations.NotNull;
 import org.junit.Test;
+
+import java.util.Random;
 
 import static org.junit.Assert.assertEquals;
 
@@ -21,6 +24,49 @@ public class MatrixTests {
 	private final float ZeroEpsilon = 0.0f;
 
 	/**
+	 * Füllt die Matrix mit zufälligen Werten
+	 * @param matrix Die Matrix
+	 */
+	private static void randomizeValues(@NotNull Matrix4 matrix) {
+		Random randomizer = new Random();
+		for (int i = 0; i < 16; ++i) {
+			matrix.setAt(i, randomizer.nextFloat());
+		}
+	}
+
+	/**
+	 * Führt den Test auf die Einheitsmatrix durch
+	 * @param a Die zu testende Matrix
+	 */
+	private void performIdentityTest(@NotNull Matrix4 a) {
+		// einsen Testen
+		assertEquals(1, a.getAt(0, 0), ZeroEpsilon);
+		assertEquals(1, a.getAt(1, 1), ZeroEpsilon);
+		assertEquals(1, a.getAt(2, 2), ZeroEpsilon);
+		assertEquals(1, a.getAt(3, 3), ZeroEpsilon);
+
+		// Nullen erste Reihe
+		assertEquals(0, a.getAt(0, 1), ZeroEpsilon);
+		assertEquals(0, a.getAt(0, 2), ZeroEpsilon);
+		assertEquals(0, a.getAt(0, 3), ZeroEpsilon);
+
+		// Nullen zweite Reihe
+		assertEquals(0, a.getAt(1, 0), ZeroEpsilon);
+		assertEquals(0, a.getAt(1, 2), ZeroEpsilon);
+		assertEquals(0, a.getAt(1, 3), ZeroEpsilon);
+
+		// Nullen dritte Reihe
+		assertEquals(0, a.getAt(2, 0), ZeroEpsilon);
+		assertEquals(0, a.getAt(2, 1), ZeroEpsilon);
+		assertEquals(0, a.getAt(2, 3), ZeroEpsilon);
+
+		// Nullen vierte Reihe
+		assertEquals(0, a.getAt(3, 0), ZeroEpsilon);
+		assertEquals(0, a.getAt(3, 1), ZeroEpsilon);
+		assertEquals(0, a.getAt(3, 2), ZeroEpsilon);
+	}
+
+	/**
 	 * Stellt sicher, dass die Matrix nach Initialisierung eine Identitätsmatrix ist.
 	 */
     @Test
@@ -28,61 +74,15 @@ public class MatrixTests {
 
 	    // Neu erzeugte Matrix testen **********************
         Matrix4 a = Matrix4.createNew();
+	    performIdentityTest(a);
 
-        // einsen Testen
-        assertEquals(1, a.getAt(0, 0), ZeroEpsilon);
-        assertEquals(1, a.getAt(1, 1), ZeroEpsilon);
-        assertEquals(1, a.getAt(2, 2), ZeroEpsilon);
-        assertEquals(1, a.getAt(3, 3), ZeroEpsilon);
-
-        // Nullen erste Reihe
-        assertEquals(0, a.getAt(0, 1), ZeroEpsilon);
-        assertEquals(0, a.getAt(0, 2), ZeroEpsilon);
-        assertEquals(0, a.getAt(0, 3), ZeroEpsilon);
-
-        // Nullen zweite Reihe
-        assertEquals(0, a.getAt(1, 0), ZeroEpsilon);
-        assertEquals(0, a.getAt(1, 2), ZeroEpsilon);
-        assertEquals(0, a.getAt(1, 3), ZeroEpsilon);
-
-        // Nullen dritte Reihe
-        assertEquals(0, a.getAt(2, 0), ZeroEpsilon);
-        assertEquals(0, a.getAt(2, 1), ZeroEpsilon);
-        assertEquals(0, a.getAt(2, 3), ZeroEpsilon);
-
-        // Nullen vierte Reihe
-        assertEquals(0, a.getAt(3, 0), ZeroEpsilon);
-        assertEquals(0, a.getAt(3, 1), ZeroEpsilon);
-        assertEquals(0, a.getAt(3, 2), ZeroEpsilon);
+	    // toIdentity() testen *****************************
+	    randomizeValues(a);
+	    a.toIdentity();
+	    performIdentityTest(a);
 
 	    // Einheitsmatrix testen ***************************
-	    a = Matrix4.UNIT;
-
-	    // einsen Testen
-	    assertEquals(1, a.getAt(0, 0), ZeroEpsilon);
-	    assertEquals(1, a.getAt(1, 1), ZeroEpsilon);
-	    assertEquals(1, a.getAt(2, 2), ZeroEpsilon);
-	    assertEquals(1, a.getAt(3, 3), ZeroEpsilon);
-
-	    // Nullen erste Reihe
-	    assertEquals(0, a.getAt(0, 1), ZeroEpsilon);
-	    assertEquals(0, a.getAt(0, 2), ZeroEpsilon);
-	    assertEquals(0, a.getAt(0, 3), ZeroEpsilon);
-
-	    // Nullen zweite Reihe
-	    assertEquals(0, a.getAt(1, 0), ZeroEpsilon);
-	    assertEquals(0, a.getAt(1, 2), ZeroEpsilon);
-	    assertEquals(0, a.getAt(1, 3), ZeroEpsilon);
-
-	    // Nullen dritte Reihe
-	    assertEquals(0, a.getAt(2, 0), ZeroEpsilon);
-	    assertEquals(0, a.getAt(2, 1), ZeroEpsilon);
-	    assertEquals(0, a.getAt(2, 3), ZeroEpsilon);
-
-	    // Nullen vierte Reihe
-	    assertEquals(0, a.getAt(3, 0), ZeroEpsilon);
-	    assertEquals(0, a.getAt(3, 1), ZeroEpsilon);
-	    assertEquals(0, a.getAt(3, 2), ZeroEpsilon);
+	    performIdentityTest(Matrix4.UNIT);
     }
 
 	/**
@@ -115,5 +115,30 @@ public class MatrixTests {
 		assertEquals(14, a.getAt(Matrix4.M42), ZeroEpsilon);
 		assertEquals(15, a.getAt(Matrix4.M43), ZeroEpsilon);
 		assertEquals(16, a.getAt(Matrix4.M44), ZeroEpsilon);
+	}
+
+	/**
+	 * Testet das Setzen und Beziehen von Werten mittels Getter und Setter
+	 */
+	@Test
+	public void setGetTest() {
+		Matrix4 a = Matrix4.createNew(false);
+
+		// Index-Accessor
+		for (int i=0; i<16; ++i) {
+			a.setAt(i, i);
+			assertEquals(i, a.getAt(i), ZeroEpsilon);
+		}
+
+		// Row-Column-Accessor
+		randomizeValues(a);
+		int i = 0;
+		for (int r = 0; r < 4; ++r) {
+			for (int c = 0; c < 4; ++c) {
+				a.setAt(r, c, i);
+				assertEquals(i, a.getAt(r, c), ZeroEpsilon);
+				++i;
+			}
+		}
 	}
 }
