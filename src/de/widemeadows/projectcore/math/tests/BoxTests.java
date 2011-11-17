a21e12a3e293d347b30e4b7509f3a78fef95127e
Markus Mayer <development@widemeadows.de>
2011-11-17 01:04:46 +0100

    Sicherstellen, dass der Extent-Vektor immer positiv ist

--- src/de/widemeadows/projectcore/math/tests/BoxTests.java
+++ src/de/widemeadows/projectcore/math/tests/BoxTests.java
@@ -1,6 +1,7 @@
 package de.widemeadows.projectcore.math.tests;
 
 import de.widemeadows.projectcore.math.Box;
+import de.widemeadows.projectcore.math.Vector3;
 import org.junit.Test;
 
 import static de.widemeadows.projectcore.math.MathUtils.DEFAULT_EPSILON;
@@ -44,4 +45,40 @@ public class BoxTests {
 
 	}
 
+	/**
+	 * Stellt sicher, dass der Extent-Vektor immer positiv ist
+	 */
+	@Test
+	public void extentAlwaysPositiveTest() {
+		// Alle Komponenten positiv
+		Box box = Box.createNew(
+				0, 0, 0,
+				1, 1, 1
+		);
+		assertEquals(1.0f, box.extent.x, DEFAULT_EPSILON);
+		assertEquals(1.0f, box.extent.y, DEFAULT_EPSILON);
+		assertEquals(1.0f, box.extent.z, DEFAULT_EPSILON);
+
+		// Alle Komponenten negativ
+		box = Box.createNew(
+				0, 0, 0,
+				-1, -1, -1
+		);
+		assertEquals(1.0f, box.extent.x, DEFAULT_EPSILON);
+		assertEquals(1.0f, box.extent.y, DEFAULT_EPSILON);
+		assertEquals(1.0f, box.extent.z, DEFAULT_EPSILON);
+
+		// Setzen der Komponenten
+		box.setExtent(-1, 1, 1);
+		assertEquals(1.0f, box.extent.x, DEFAULT_EPSILON);
+		assertEquals(1.0f, box.extent.y, DEFAULT_EPSILON);
+		assertEquals(1.0f, box.extent.z, DEFAULT_EPSILON);
+
+		// Setzen der Komponenten
+		box.setExtent(Vector3.createNew(1, -1, -1));
+		assertEquals(1.0f, box.extent.x, DEFAULT_EPSILON);
+		assertEquals(1.0f, box.extent.y, DEFAULT_EPSILON);
+		assertEquals(1.0f, box.extent.z, DEFAULT_EPSILON);
+	}
+	
 }
